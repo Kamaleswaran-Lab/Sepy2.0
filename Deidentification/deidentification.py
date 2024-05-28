ee3abc156df5cc8caf2a831845d33f7b8bfc4fd6
MehakArora <maror24@bignode1.priv.bmi.emory.edu>
2024-05-28 19:06:22 -0400

    Deidentiication scripts

--- Deidentification/deidentification.py
+++ Deidentification/deidentification.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 
 hash_key = '123'
 
-YEARS = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+YEARS = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
 
 def hash_value(value, hash_key):
     return hashlib.sha256((str(value) + hash_key).encode()).hexdigest()
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     year = YEARS[args.index]
 
-    path_to_data = Path('/labs/collab/K-lab-MODS/MODS-PHI/Emory_Data/')
-    path_to_deid_data = Path('/labs/kamaleswaranlab/MODS/Data/deid/')
+    path_to_data = Path('/labs/collab/K-lab-MODS/MODS-PHI/Grady_Data/')
+    path_to_deid_data = Path('/labs/kamaleswaranlab/MODS/Data/deid_grady/')
 
     path_to_data = path_to_data / str(year)
     path_to_deid_data = path_to_deid_data / str(year)
@@ -34,20 +34,25 @@ if __name__ == "__main__":
             matching_list['pat_id'] = df['pat_id'].unique()
             matching_list['pat_id_deid'] = matching_list['pat_id'].apply(lambda x: hash_value(x, hash_key))
             matching_list.to_csv(path_to_data / 'matching_list_patid.csv', index = False)
-            
+            print("Pat ID matching list saved")   
             matching_list = pd.DataFrame( columns= ['csn', 'csn_deid'])
             matching_list['csn'] = df['csn'].unique()
             matching_list['csn_deid'] = matching_list['csn'].apply(lambda x: hash_value(x, hash_key))
             matching_list.to_csv(path_to_data / 'matching_list_csn.csv', index = False)
+            print("CSN matching list saved")
 
         # Deidentify identifier columns
         for column in identifier_columns:
             try:
-                df[column] = df[column].apply(lambda x: hash_value(x, hash_key))
+                if column in df.columns:
+                    df[column] = df[column].apply(lambda x: hash_value(x, hash_key))
+                else:
+                    column = column.upper()
+                    df[column] = df[column].apply(lambda x: hash_value(x, hash_key))
             except KeyError:
-                column = column.upper()
-                df[column] = df[column].apply(lambda x: hash_value(x, hash_key))
-        
+                print(column + ' does not exist in ' + file.name)
+         
+        print(file.name + " deidentified")
         if 'DEMOGRAPHICS' in file.name:
             #Drop Columns
             try:
@@ -55,7 +60,8 @@ if __name__ == "__main__":
             except KeyError:
                 drop_columns = [column.upper() for column in drop_columns]
                 df.drop(columns = drop_columns, inplace = True)
-        
+            print("Demographic columns dropped")
+
         df.to_csv(path_to_deid_data / file.name, index = False, sep = '|')
         del df 
 
