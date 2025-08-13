57ec2754d9f22ef8d21c5682f35f42ae69537fd7
MehakArora <mehak.arora@duke.edu>
2025-08-13 06:43:56 -0400

    processing fluids for oddjobs

--- slurmjobs/fluids.sh
+++ slurmjobs/fluids.sh
@@ -4,8 +4,11 @@
 #SBATCH --ntasks=1 
 #SBATCH --mem=64G
 #SBATCH --time=72:00:00
-#SBATCH --array=0-9
+#SBATCH --array=0-12
 #SBATCH --output=./output/fluids_%j_%a.out
 #SBATCH --error=./errors/fluids_%j_%a.err
+
+filepath=
+savepath=/labs/collab/K-lab-MODS/MODS-PHI/Emory_Data/INs
 
 python $HOME/Sepy/process_fluids.py --chunks 10 --index $SLURM_ARRAY_TASK_ID
